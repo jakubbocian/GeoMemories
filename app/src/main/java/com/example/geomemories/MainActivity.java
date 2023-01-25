d3954affed208fa0d44d722fd8b49f0ce84e2240
dago07 <82581656+dago07@users.noreply.github.com>
2023-01-25 23:22:13 +0100

    grafica

--- app/src/main/java/com/example/geomemories/MainActivity.java
+++ app/src/main/java/com/example/geomemories/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.geomemories;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,16 +13,35 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.geomemories.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        /*binding.bottomNavigationView.setOnMenuItemSelectListener(item ->{
+
+
+            switch (item.getItemId()) {
+                case R.id.person:
+                    replaceFragment(new profilefragment());
+                    break;
+                case R.id.home:
+                    replaceFragment(new settingsfragment());
+                    break;
+
+            }
+            return;
+        });*/
 
 
         mAuth = FirebaseAuth.getInstance();
@@ -40,5 +62,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });*/
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
